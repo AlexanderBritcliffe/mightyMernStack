6a61cd46c483b06e9a4a139186c13571d257a08c
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-09-09 19:06:15 -0400

    added delet user profile id

--- models/Profile.js
+++ models/Profile.js
@@ -57,6 +57,37 @@ const ProfileSchema = new mongoose.Schema({
       }
     }
   ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date,
+        required: true
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String,
+      }
+    }
+  ],
   social: {
     youtube: {
       type: String
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -69,8 +69,103 @@ router.post(
 
     const profileFields = {};
     profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id })
+
+    if(profile) { //if profile is found
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true }
+      );
+
+      return res.json(profile)
+    }
+    //Create profile
+    profile = new Profile(profileFields);
+
+    await profile.save();
+    res.json(profile);
+
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+    }
   }
 );
+
+//@route GET api/profile
+//@desc Get all profiles
+//@access Public
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route GET api/profile/user/:user_id
+//@desc Get profile by user id
+//@access Public
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate('user',
+    ['name', 'avatar']);
+    //check to make sure profile for a given user
+    if(!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch(err) {
+    console.error(err.message);
+    if(err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+//req.params.user_id is coming from the url on line 135
+
+//@route DELETE api/profile
+//@desc Delete profile user and posts
+//@access Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //remove user
+    await Profile.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
 
