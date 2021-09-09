47a9e459185cdcd8c207ea94eb43e8f12f665b1d
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-09-08 22:49:53 -0400

    finished authentication

---
+++ routes/api/profile.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+//@route GET api/profile/me
+//@desc get current users profile
+//@access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user '});
+    }
+
+    res.json(profile);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route POST api/profile/me
+//@desc create or update user profile
+//@access Private
+
+router.post('/',
+ [
+   auth,
+   [
+     check('status', 'Status is required')
+     .not()
+     .isEmpty(),
+     check('skills', 'Skills is required')
+     .not()
+     .isEmpty()
+   ]
+ ],
+ async (req, res) => {
+  const errors
+})
+
+module.exports = router;
+
+//user pertains to profile model user field at top which is the object id of the user on line 13
+//populate grabs array of fields we want from user model
